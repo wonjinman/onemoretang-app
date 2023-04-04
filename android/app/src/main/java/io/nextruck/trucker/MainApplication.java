7c50d95cb8f3d3878a619248fcb9ebf5171dc69a
deer-chiho <chiho.kim@deering.co>
2023-04-04 17:29:27 +0900

    Fix: import 누락 제거

--- android/app/src/main/java/io/nextruck/trucker/MainApplication.java
+++ android/app/src/main/java/io/nextruck/trucker/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
